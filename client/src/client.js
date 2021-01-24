a8321c618c149bf90cf60ac133ff1ae5281f8e55
Omar El Tarzi <omartarzi@gmail.com>
2021-01-23 20:12:39 -0800

    Removed test.

--- client/src/client.js
+++ client/src/client.js
@@ -2,7 +2,6 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
-t;
 
 const client = new ApolloClient();
 
