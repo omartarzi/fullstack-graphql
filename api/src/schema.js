dec798240b20ba046fecdcfae406d21c30bf7534
Omar El Tarzi <omartarzi@gmail.com>
2021-01-24 05:59:01 +0200

    Update schema.js

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,43 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet]!
+  }
 
+  type Pet {
+    id: String!
+    createdAt: String!
+    name: String!
+    type: String!
+    img: String
+    buddies: [Pet]
+    owner: User!
+  }
+
+  input PetInput {
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput): Pet
+  }
+
+  input NewPetInput {
+    name: String!
+    type: String!
+  }
+
+  type Mutation {
+    newPet(input: NewPetInput!): Pet!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
