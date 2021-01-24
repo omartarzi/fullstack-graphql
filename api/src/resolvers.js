567dfc29511f515ed7f697b9673d70a80c2e4b90
Omar El Tarzi <omartarzi@gmail.com>
2021-01-24 05:58:39 +0200

    Update resolvers.js

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,33 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input);
+    },
   },
   Mutation: {
-    
+    newPet(_, { input }, ctx) {
+      const pet = ctx.models.Pet.create(input);
+      return pet;
+    },
   },
   Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+    owner(_, __, ctx) {
+      // ctx.models.User.findById(pet.user)
+      return ctx.models.User.findOne();
+    },
+    //   img(pet) {
+    //     return pet.type === 'DOG'
+    //       ? 'https://placedog.net/300/300'
+    //       : 'http://placekitten.com/300/300'
+    //   }
   },
   User: {
-    
-  }
-}
+    pets(user, __, ctx) {
+      return ctx.models.Pet.findMany({ user: user.id });
+    },
+  },
+};
