9e19367e18a3b5e3ae3712443b16a300cb4b5a17
Omar El Tarzi <omartarzi@gmail.com>
2021-01-24 12:23:39 -0800

    Completed Apollo React.

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,47 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+const NEW_PET = gql`
+  mutation CreateAPet($newPet:NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      type
+      name
+      img
+    }
+  }
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const {data, loading, error} = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(NEW_PET)
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {newPet: input}
+    })
   }
-  
+
+  if (loading || newPet.loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>error!</p>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +64,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
